4138178d7af9e1c9f388409b445339a5edf84f55
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-28 21:01:17 +0800

    test: add unit tests for swagger and 404 route

--- server/index.test.ts
+++ server/index.test.ts
@@ -10,4 +10,19 @@ describe("Elysia", () => {
 
     expect(response).toBe("Hello Elysia");
   });
+
+  it("return a 404 response", async () => {
+    const response = await app
+      .handle(new Request("http://localhost/404"))
+      .then(async (res) => res.text());
+
+    expect(response).toBe("NOT_FOUND");
+  });
+
+  it("should access the swagger  ui", async () => {
+    const response = await app
+      .handle(new Request("http://localhost/swagger"))
+      .then(async (res) => res.text());
+    expect(response).toContain("Elysia API Documentation");
+  });
 });
